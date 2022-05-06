a2e21b246a9e1c17cf36dd390ad12fad6183dda0
Furkan Yazar <furkanyazar9853@gmail.com>
2022-05-06 19:55:31 +0300

    Add project files.

---
+++ Core/Business/EntityManager.cs
@@ -0,0 +1,36 @@
+﻿using Core.DataAccess;
+using Core.Entities.Abstract;
+using System.Collections.Generic;
+
+namespace Core.Business
+{
+    public class EntityManager<TEntity, TDal> : IEntityService<TEntity> where TEntity : class, IEntity, new() where TDal : IEntityRepository<TEntity>
+    {
+        public TDal _tdal;
+
+        public EntityManager(TDal tdal)
+        {
+            _tdal = tdal;
+        }
+
+        public void Add(TEntity entity)
+        {
+            _tdal.Add(entity);
+        }
+
+        public void Delete(TEntity entity)
+        {
+            _tdal.Delete(entity);
+        }
+
+        public List<TEntity> GetAll()
+        {
+            return _tdal.GetAll();
+        }
+
+        public void Update(TEntity entity)
+        {
+            _tdal.Update(entity);
+        }
+    }
+}
---
+++ Core/Business/IEntityService.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities.Abstract;
+using System.Collections.Generic;
+
+namespace Core.Business
+{
+    public interface IEntityService<T> where T : class, IEntity, new()
+    {
+        List<T> GetAll();
+
+        void Add(T entity);
+
+        void Update(T entity);
+
+        void Delete(T entity);
+    }
+}
