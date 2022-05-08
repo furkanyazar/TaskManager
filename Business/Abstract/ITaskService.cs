bd673a09510083c0dc5e4a807a68e3dc2b47128f
Furkan Yazar <furkanyazar9853@gmail.com>
2022-05-08 13:37:18 +0300

    Delete task type and code refactoring

--- Business/Abstract/ITaskService.cs
+++ Business/Abstract/ITaskService.cs
@@ -6,7 +6,7 @@ namespace Business.Abstract
 {
     public interface ITaskService : IEntityService<Task>
     {
-        List<Task> GetAllByTypeIdAndUserId(int typeId, int userId);
+        List<Task> GetAllByUserId(int userId);
 
         Task GetById(int id);
     }
---
+++ DataAccess/Migrations/20220507220645_mig_delete_task_type.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccess.Migrations
+{
+    public partial class mig_delete_task_type : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_TaskTypes_TaskTypeId",
+                table: "Tasks");
+
+            migrationBuilder.DropTable(
+                name: "TaskTypes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tasks_TaskTypeId",
+                table: "Tasks");
+
+            migrationBuilder.DropColumn(
+                name: "TaskTypeId",
+                table: "Tasks");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TaskTypeId",
+                table: "Tasks",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "TaskTypes",
+                columns: table => new
+                {
+                    TaskTypeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Status = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TaskTypes", x => x.TaskTypeId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tasks_TaskTypeId",
+                table: "Tasks",
+                column: "TaskTypeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_TaskTypes_TaskTypeId",
+                table: "Tasks",
+                column: "TaskTypeId",
+                principalTable: "TaskTypes",
+                principalColumn: "TaskTypeId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
